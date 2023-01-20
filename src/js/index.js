c6aed40f1ed86160ad0bf70de9afa077fc0d86ec
Lucas Soares <soares_lucas13@hotmail.com>
2023-01-19 21:26:11 -0300

    javascript: 📜 preLoader

--- src/js/index.js
+++ src/js/index.js
@@ -12,18 +12,18 @@ showmenu = () => {
 
 
 // PRE LOADER 
-// const preloader = document.querySelector('.preloader');
-// const fadeOutEffect = setInterval(() => {
-//     if (!preloader.style.opacity) {
-//       preloader.style.opacity = 1;
-//     }
-//     if (preloader.style.opacity > 0) {
-//       preloader.style.opacity -= 0.1;
-//     } else {
-//       clearInterval(fadeOutEffect);
-//       preloader.style.display = 'none';
-//     }
-//   }, 500);
+const preloader = document.querySelector('.preloader');
+const fadeOutEffect = setInterval(() => {
+    if (!preloader.style.opacity) {
+      preloader.style.opacity = 1;
+    }
+    if (preloader.style.opacity > 0) {
+      preloader.style.opacity -= 0.1;
+    } else {
+      clearInterval(fadeOutEffect);
+      preloader.style.display = 'none';
+    }
+  }, 500);
   
 
 
